ed2ccb13db29f751c1c0c3616fabf20005e98273
hitheshamin24 <hitheshamin423@gmail.com>
2025-05-30 20:11:10 +0530

    30-5-2025 s commit

--- backend/index.js
+++ backend/index.js
@@ -8,18 +8,15 @@ app.use(express.json());
 
 app.post("/api/recommend", async (req, res) => {
   try {
-    const mlResponse = await axios.post(
-      "http://localhost:8000/predict",
-      req.body
-    );
+    const mlResponse = await axios.post("http://localhost:8000/predict", req.body);
     res.json(mlResponse.data);
   } catch (error) {
     console.error(error.message);
     res.status(500).json({ error: "ML server error" });
   }
 });
+
 const PORT = 5000;
 app.listen(PORT, () => {
   console.log(`Backend server running on port ${PORT}`);
-  
 });
