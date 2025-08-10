ff5df0c2b9d6ac3f8b7a423134798a1e328c8273
hitheshamin24 <hitheshamin423@gmail.com>
2025-08-10 19:33:34 +0530

    updated frontend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -6,18 +6,43 @@ import Footer from "../components/Footer";
 import Form from "./Form"; // ✅ import your form from pages
 import { SignedIn } from "@clerk/clerk-react";
 import { useRef } from "react";
+import React, { useEffect } from "react";
+import { useUser, useAuth } from "@clerk/clerk-react";
+import axios from "axios";
 
 const Home = () => {
   const formRef = useRef(null);
+  const { isSignedIn } = useUser();
+  const { getToken } = useAuth();
+  useEffect(() => {
+    if (!isSignedIn) return;
 
+    async function saveUser() {
+      try {
+        const token = await getToken();
+        const res = await axios.post(
+          "http://localhost:5000/api/user",
+          {},
+          { headers: { Authorization: `Bearer ${token}` } }
+        );
+        console.log("User saved or already exists");
+        console.log("Backend response:", res.data);
+      } catch (error) {
+        console.error("Error saving user:", error);
+      }
+    }
+
+    saveUser();
+  }, [isSignedIn, getToken]);
   const scrollToForm = () => {
     formRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
   return (
     <>
-      <Navbar onGetStartedClick={scrollToForm} /> {/* Button will scroll down */}
-      <Hero onGetStartedClick={scrollToForm} />   {/* Optional */}
+      <Navbar onGetStartedClick={scrollToForm} />{" "}
+      {/* Button will scroll down */}
+      <Hero onGetStartedClick={scrollToForm} /> {/* Optional */}
       <About />
       <HowItWorks />
       <div ref={formRef}>
